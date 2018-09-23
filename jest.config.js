67c93bd10a4f55df80526b9b8d3832a5f16856e6
Kári Frederiksen <karifrederiksen@gmail.com>
2018-09-24 00:16:39 +0100

    remove custom node resolution paths

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    moduleNameMapper: {
-        "canvas/(.*)$": "<rootDir>/src/canvas/$1",
-        "ui/(.*)$": "<rootDir>/src/ui/$1",
-    },
+    moduleNameMapper: {},
 }
