3bd5a4b0f2ff2d93c560f4e0259c2be65163dbe0
Kári Frederiksen <karifrederiksen@gmail.com>
2018-10-03 22:08:54 +0100

    add PCG RNG implementation

--- src/rng.ts
+++ src/rng.ts
@@ -1,37 +1,70 @@
 import { T2, T3, T4 } from "./util"
 
-/*
-  Pseudo-random number generator
-  Meant for testing purposes, so I can get the same pattern every time I test something using random numbers.
-  https://en.wikipedia.org/wiki/Xorshift
-  Implementation of Xorshift 128
-*/
+namespace Pcg {
+    /*
+     * Port of Elm's PCG RNG implementation
+     * https://github.com/elm/random/blob/1.0.0/src/Random.elm
+     * http://www.pcg-random.org/
+     */
+    export type Seed = T2<number, number>
 
-export type Seed = T4<number, number, number, number>
-
-export function seed(n: number): Seed {
-    let seed_: Seed = [n, 0, 0, 0]
-    for (let i = 0; i < 32; i++) {
-        const [_, nextSeed] = nextInt(seed_)
-        seed_ = nextSeed
+    export function seed(x: number) {
+        const [state, incr] = step([0, 1013904223])
+        return step([(state + x) >>> 0, incr])
     }
-    return seed_
+
+    function step([state, incr]: Seed): Seed {
+        return [(state * 1664525 + incr) >>> 0, incr]
+    }
+
+    function output([state]: Seed): number {
+        const word = (state ^ (state >>> ((state >>> 28) + 4))) * 277803737
+        return ((word >>> 22) ^ word) >>> 0
+    }
+
+    export function nextInt(s: Seed): T2<number, Seed> {
+        return [output(s), step(s)]
+    }
 }
 
-export function nextInt(state: Seed): T2<number, Seed> {
-    const s = state[0]
-    let t = state[3]
-    t ^= t << 11
-    t ^= t >>> 8
-    t ^= s
-    t ^= s >>> 19
-    return [t, [t, s, state[1], state[2]]]
+namespace Xorshift {
+    /*
+     * Implementation of Xorshift 128
+     * https://en.wikipedia.org/wiki/Xorshift
+     */
+
+    export type Seed = T4<number, number, number, number>
+
+    export function seed(n: number): Seed {
+        let seed_: Seed = [n, 0, 0, 0]
+        for (let i = 0; i < 32; i++) {
+            const [_, nextSeed] = nextInt(seed_)
+            seed_ = nextSeed
+        }
+        return seed_
+    }
+
+    export function nextInt(state: Seed): T2<number, Seed> {
+        const s = state[0]
+        let t = state[3]
+        t ^= t << 11
+        t ^= t >>> 8
+        t ^= s
+        t ^= s >>> 19
+        return [t, [t, s, state[1], state[2]]]
+    }
 }
+
+export type Seed = Pcg.Seed
+
+export const seed = Pcg.seed
+
+export const nextInt = Pcg.nextInt
 
 export function next(state: Seed): T2<number, Seed> {
     const [result, nextState] = nextInt(state)
-    console.log(result)
-    return [(result >>> 0) / ((1 << 30) * 2), nextState]
+    const floatResult = (result >>> 0) / 4294967295
+    return [floatResult, nextState]
 }
 
 export function next2(state: Seed): T3<number, number, Seed> {
