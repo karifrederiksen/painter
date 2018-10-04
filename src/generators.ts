30538c1d6838754010565f835ec32fa7d63fad3b
Kári Frederiksen <karifrederiksen@gmail.com>
2018-10-04 20:41:08 +0100

    add fancy generators

---
+++ src/generators.ts
@@ -0,0 +1,197 @@
+import * as Rng from "./rng"
+import { T2, T3 } from "./util"
+
+type GenResult<a> = T2<a, Rng.Seed>
+
+export interface Generator<a> {
+    (rng: Rng.Seed): T2<a, Rng.Seed>
+}
+
+export function always<a>(val: a): Generator<a> {
+    return rng => [val, rng]
+}
+
+export function bool(rng: Rng.Seed): GenResult<boolean> {
+    const [pct, nextSeed] = Rng.next(rng)
+    return [pct < 5, nextSeed]
+}
+
+export function int(min: number, max: number): Generator<number> {
+    min = min | 0
+    max = max | 0
+    return rng => int_(min, max, rng)
+}
+
+function int_(min: number, max: number, rng: Rng.Seed): GenResult<number> {
+    const [pct, nextSeed] = Rng.next(rng)
+    const delta = max - min
+    return [pct * delta + min, nextSeed]
+}
+
+export function float(min: number, max: number): Generator<number> {
+    return rng => float_(min, max, rng)
+}
+
+function float_(min: number, max: number, rng: Rng.Seed): GenResult<number> {
+    const [pct, nextSeed] = Rng.next(rng)
+    const delta = max - min
+    return [pct * delta + min, nextSeed]
+}
+
+export function t2<a, b>(genA: Generator<a>, genB: Generator<b>): Generator<T2<a, b>> {
+    return rng => t2_(genA, genB, rng)
+}
+
+function t2_<a, b>(genA: Generator<a>, genB: Generator<b>, rng0: Rng.Seed): GenResult<T2<a, b>> {
+    const [valA, rng1] = genA(rng0)
+    const [valB, rng2] = genB(rng1)
+    return [[valA, valB], rng2]
+}
+
+export function t3<a, b, c>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    genC: Generator<c>
+): Generator<T3<a, b, c>> {
+    return rng => t3_(genA, genB, genC, rng)
+}
+
+function t3_<a, b, c>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    genC: Generator<c>,
+    rng0: Rng.Seed
+): GenResult<T3<a, b, c>> {
+    const [valA, rng1] = genA(rng0)
+    const [valB, rng2] = genB(rng1)
+    const [valC, rng3] = genC(rng2)
+    return [[valA, valB, valC], rng3]
+}
+
+export function array<a>(length: number, gen: Generator<a>): Generator<Array<a>> {
+    return rng => array_(length, gen, rng)
+}
+
+function array_<a>(length: number, gen: Generator<a>, rng: Rng.Seed): GenResult<Array<a>> {
+    const arr = new Array(length)
+    for (let i = 0; i < length; i++) {
+        const [val, nextRng] = gen(rng)
+        arr[i] = val
+        rng = nextRng
+    }
+    return [arr, rng]
+}
+
+export type ObjectProps<a> = { readonly [propName in keyof a]: Generator<a[propName]> }
+
+export function object<a>(gens: ObjectProps<a>): Generator<a> {
+    return rng => object_(gens, rng)
+}
+
+function object_<a>(gens: ObjectProps<a>, rng: Rng.Seed): GenResult<a> {
+    const obj = {} as a
+    for (const prop in gens) {
+        const [val, nextRng] = gens[prop](rng)
+        obj[prop] = val
+        rng = nextRng
+    }
+    return [obj, rng]
+}
+
+export function map<a, b>(genA: Generator<a>, f: (valA: a) => b): Generator<b> {
+    return rng => map_(genA, f, rng)
+}
+
+function map_<a, b>(genA: Generator<a>, f: (valA: a) => b, rng0: Rng.Seed): GenResult<b> {
+    const [valA, rng1] = genA(rng0)
+    return [f(valA), rng1]
+}
+
+export function map2<a, b, c>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    f: (valA: a, valB: b) => c
+): Generator<c> {
+    return rng => map2_(genA, genB, f, rng)
+}
+
+function map2_<a, b, c>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    f: (valA: a, valB: b) => c,
+    rng0: Rng.Seed
+): GenResult<c> {
+    const [valA, rng1] = genA(rng0)
+    const [valB, rng2] = genB(rng1)
+    return [f(valA, valB), rng2]
+}
+
+export function map3<a, b, c, d>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    genC: Generator<c>,
+    f: (valA: a, valB: b, valC: c) => d
+): Generator<d> {
+    return rng => map3_(genA, genB, genC, f, rng)
+}
+
+function map3_<a, b, c, d>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    genC: Generator<c>,
+    f: (valA: a, valB: b, valC: c) => d,
+    rng0: Rng.Seed
+): GenResult<d> {
+    const [valA, rng1] = genA(rng0)
+    const [valB, rng2] = genB(rng1)
+    const [valC, rng3] = genC(rng2)
+    return [f(valA, valB, valC), rng3]
+}
+
+export function map4<a, b, c, d, e>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    genC: Generator<c>,
+    genD: Generator<d>,
+    f: (valA: a, valB: b, valC: c, valD: d) => e
+): Generator<e> {
+    return rng => map4_(genA, genB, genC, genD, f, rng)
+}
+
+function map4_<a, b, c, d, e>(
+    genA: Generator<a>,
+    genB: Generator<b>,
+    genC: Generator<c>,
+    genD: Generator<d>,
+    f: (valA: a, valB: b, valC: c, valD: d) => e,
+    rng0: Rng.Seed
+): GenResult<e> {
+    const [valA, rng1] = genA(rng0)
+    const [valB, rng2] = genB(rng1)
+    const [valC, rng3] = genC(rng2)
+    const [valD, rng4] = genD(rng3)
+    return [f(valA, valB, valC, valD), rng4]
+}
+
+export function andThen<a, b>(genA: Generator<a>, f: (val: a) => Generator<b>): Generator<b> {
+    return rng => andThen_(genA, f, rng)
+}
+
+function andThen_<a, b>(
+    genA: Generator<a>,
+    f: (val: a) => Generator<b>,
+    rng0: Rng.Seed
+): GenResult<b> {
+    const [valA, rng1] = genA(rng0)
+    return f(valA)(rng1)
+}
+
+export function lazy<a>(createGen: () => Generator<a>): Generator<a> {
+    let cachedGen: Generator<a> | null = null
+    return rng => {
+        if (cachedGen === null) {
+            cachedGen = createGen()
+        }
+        return cachedGen(rng)
+    }
+}
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,5 @@
 import * as Color from "./color"
-import * as Rng from "./rng"
-import { T2 } from "./util"
+import * as Gen from "./generators"
 
 export interface Theme {
     readonly color: {
@@ -27,76 +26,43 @@ export interface Theme {
     }
 }
 
-interface ThemeBuilder {
-    color: {
-        primary: Color.Hsluv
-        primaryLight: Color.Hsluv
-        primaryDark: Color.Hsluv
-        secondary: Color.Hsluv
-        secondaryLight: Color.Hsluv
-        secondaryDark: Color.Hsluv
-        onPrimary: Color.Hsluv
-        onSecondary: Color.Hsluv
-        onSurface: Color.Hsluv
-        surface: Color.Hsluv
-        background: Color.Hsluv
-    }
-    shadows: {
-        button: string
-        surface: string
-        menu: string
-    }
-    fonts: {
-        normal: string
-        monospace: string
-    }
-}
+const hsluvGen = (maxSat: number, luminance: number) =>
+    Gen.map3(
+        Gen.float(0, 360),
+        Gen.float(0, maxSat),
+        Gen.always(luminance),
+        (h, s, l) => new Color.Hsluv(h, s, l)
+    )
 
-const makeBuilder = (): ThemeBuilder => ({
-    color: {
-        primary: new Color.Hsluv(0, 100, 80),
-        primaryLight: new Color.Hsluv(0, 100, 80),
-        primaryDark: new Color.Hsluv(0, 100, 80),
-        secondary: new Color.Hsluv(0, 100, 77),
-        secondaryLight: new Color.Hsluv(0, 100, 77),
-        secondaryDark: new Color.Hsluv(0, 100, 77),
-        background: new Color.Hsluv(0, 20, 46),
-        surface: new Color.Hsluv(0, 35, 38),
-        onPrimary: new Color.Hsluv(0, 100, 0),
-        onSecondary: new Color.Hsluv(0, 100, 0),
-        onSurface: new Color.Hsluv(0, 100, 100),
-    },
-    shadows: {
-        button:
-            "0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12)",
-        surface:
-            "0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12)",
-        menu: "2px 2px 2px rgba(0, 0, 0, 0.45), 0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-    },
-    fonts: {
-        normal: `"Helvetica", "Arial", sans-serif`,
-        monospace: `"Courier New", Courier, monospace`,
-    },
+const colorsGen = Gen.object({
+    primary: hsluvGen(100, 80),
+    primaryLight: hsluvGen(100, 80),
+    primaryDark: hsluvGen(100, 80),
+    secondary: hsluvGen(100, 77),
+    secondaryLight: hsluvGen(100, 77),
+    secondaryDark: hsluvGen(100, 77),
+    background: hsluvGen(20, 46),
+    surface: hsluvGen(35, 38),
+    onPrimary: hsluvGen(100, 0),
+    onSecondary: hsluvGen(100, 0),
+    onSurface: hsluvGen(100, 100),
 })
 
-function randomizeColors(rng_: Rng.Seed, builder: ThemeBuilder): T2<Theme, Rng.Seed> {
-    const rng = new Rng.StatefulWrapper(rng_)
+const shadowsGen = Gen.always({
+    button:
+        "0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12)",
+    surface:
+        "0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12)",
+    menu: "2px 2px 2px rgba(0, 0, 0, 0.45), 0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+})
 
-    const generate = ({ s, l }: Color.Hsluv) => new Color.Hsluv(rng.next() * 360, rng.next() * s, l)
+const fontsGen = Gen.always({
+    normal: `"Helvetica", "Arial", sans-serif`,
+    monospace: `"Courier New", Courier, monospace`,
+})
 
-    builder.color.primary = generate(builder.color.primary)
-    builder.color.primaryLight = generate(builder.color.primaryLight)
-    builder.color.primaryDark = generate(builder.color.primaryDark)
-    builder.color.secondary = generate(builder.color.secondary)
-    builder.color.secondaryLight = generate(builder.color.secondaryLight)
-    builder.color.secondaryDark = generate(builder.color.secondaryDark)
-    builder.color.background = generate(builder.color.background)
-    builder.color.surface = generate(builder.color.surface)
-    builder.color.onPrimary = generate(builder.color.onPrimary)
-    builder.color.onSecondary = generate(builder.color.onSecondary)
-    builder.color.onSurface = generate(builder.color.onSurface)
-
-    return [builder, rng.state]
-}
-
-export const random = (rng: Rng.Seed) => randomizeColors(rng, makeBuilder())
+export const random = Gen.object<Theme>({
+    color: colorsGen,
+    shadows: shadowsGen,
+    fonts: fontsGen,
+})
