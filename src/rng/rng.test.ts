ad2e214540b596528aa19b0ed108d64e2234c595
Kari Frederiksen <karifrederiksen@gmail.com>
2019-09-12 22:29:56 +0100

    switch to ava for tests

--- src/rng/rng.test.ts
+++ src/rng/rng.test.ts
@@ -1,52 +1,51 @@
+import test from "ava"
 import * as Rng from "./rng"
 
-describe("rng.xorshift", () => {
-    const seeds: readonly Rng.Seed[] = [
-        Rng.XorshiftSeed.create(0),
-        Rng.XorshiftSeed.create(1),
-        Rng.XorshiftSeed.create(2),
-        Rng.XorshiftSeed.create(3),
-        Rng.XorshiftSeed.create(4),
-        Rng.XorshiftSeed.create(5),
-        Rng.XorshiftSeed.create(-1 >>> 0),
-        Rng.XorshiftSeed.create(-1),
-    ]
+const seeds: readonly Rng.Seed[] = [
+    Rng.XorshiftSeed.create(0),
+    Rng.XorshiftSeed.create(1),
+    Rng.XorshiftSeed.create(2),
+    Rng.XorshiftSeed.create(3),
+    Rng.XorshiftSeed.create(4),
+    Rng.XorshiftSeed.create(5),
+    Rng.XorshiftSeed.create(-1 >>> 0),
+    Rng.XorshiftSeed.create(-1),
+]
 
-    test("(nextInt) returns an integer in range 0 to ((2 ** 32) - 1)", () => {
-        seeds.forEach(seed => {
-            const [val] = seed.nextInt()
-            expect(val).toBeGreaterThanOrEqual(0)
-            expect(val).toBeLessThanOrEqual(2 ** 32 - 1)
-        })
+test("(nextInt) returns an integer in range 0 to ((2 ** 32) - 1)", t => {
+    seeds.forEach(seed => {
+        const [val] = seed.nextInt()
+        t.assert(val >= 0)
+        t.assert(val <= 2 ** 32 - 1)
     })
+})
 
-    test("(next) returns a float in range 0 to 1", () => {
-        seeds.forEach(seed => {
-            const [val] = seed.nextFloat()
-            expect(val).toBeGreaterThanOrEqual(0)
-            expect(val).toBeLessThanOrEqual(1)
-        })
+test("(next) returns a float in range 0 to 1", t => {
+    seeds.forEach(seed => {
+        const [val] = seed.nextFloat()
+        t.assert(val >= 0)
+        t.assert(val <= 1)
     })
+})
 
-    test("(next2) returns two floats in range 0 to 1", () => {
-        seeds.forEach(seed => {
-            const [val1, val2] = seed.next2Floats()
-            expect(val1).toBeGreaterThanOrEqual(0)
-            expect(val1).toBeLessThanOrEqual(1)
-            expect(val2).toBeGreaterThanOrEqual(0)
-            expect(val2).toBeLessThanOrEqual(1)
-        })
+test("(next2) returns two floats in range 0 to 1", t => {
+    seeds.forEach(seed => {
+        const [val1, val2] = seed.next2Floats()
+        t.assert(val1 >= 0)
+        t.assert(val1 <= 1)
+        t.assert(val2 >= 0)
+        t.assert(val2 <= 1)
     })
+})
 
-    test("(next3) returns three floats in range 0 to 1", () => {
-        seeds.forEach(seed => {
-            const [val1, val2, val3] = seed.next3Floats()
-            expect(val1).toBeGreaterThanOrEqual(0)
-            expect(val1).toBeLessThanOrEqual(1)
-            expect(val2).toBeGreaterThanOrEqual(0)
-            expect(val2).toBeLessThanOrEqual(1)
-            expect(val3).toBeGreaterThanOrEqual(0)
-            expect(val3).toBeLessThanOrEqual(1)
-        })
+test("(next3) returns three floats in range 0 to 1", t => {
+    seeds.forEach(seed => {
+        const [val1, val2, val3] = seed.next3Floats()
+        t.assert(val1 >= 0)
+        t.assert(val1 <= 1)
+        t.assert(val2 >= 0)
+        t.assert(val2 <= 1)
+        t.assert(val3 >= 0)
+        t.assert(val3 <= 1)
     })
 })
--- src/tools/brushDelay.test.ts
+++ src/tools/brushDelay.test.ts
@@ -1,82 +1,81 @@
+import test from "ava"
 import * as Delay from "./brushDelay"
 
 function mkInput(x: number, y: number, pressure: number): Delay.Input {
     return new Delay.Input(x, y, pressure)
 }
 
-describe("brushDelay", () => {
-    const state: Delay.State = { startTime: 0, start: mkInput(0, 0, 0), end: mkInput(100, 100, 1) }
+const state: Delay.State = { startTime: 0, start: mkInput(0, 0, 0), end: mkInput(100, 100, 1) }
 
-    test("(update) should produce state.end when delay is 0", () => {
+test("(update) should produce state.end when delay is 0", t => {
+    for (let time = 0; time < 10; time++) {
+        t.deepEqual(Delay.update(Delay.delay(0), state, time)[1], state.end)
+    }
+})
+
+test("(update) should produce state.start when time is equal to startTime", t => {
+    t.deepEqual(Delay.update(Delay.delay(1), state, state.startTime)[1], state.start)
+})
+
+test("(update) should produce state.end when time is greater than or equal to (startTime + delay)", t => {
+    for (let delay = 0; delay < 10; delay++) {
         for (let time = 0; time < 10; time++) {
-            expect(Delay.update(Delay.delay(0), state, time)[1]).toEqual(state.end)
+            const startTime = state.startTime + time + delay
+            t.deepEqual(Delay.update(Delay.delay(delay), state, startTime)[1], state.end)
         }
-    })
+    }
+})
 
-    test("(update) should produce state.start when time is equal to startTime", () => {
-        expect(Delay.update(Delay.delay(1), state, state.startTime)[1]).toEqual(state.start)
-    })
+test("(update) update should produce values between start and end when time is between (startTime) and (startTime + delay)", t => {
+    // we can't test for the absolute values since the easing function's implementation should remain flexible.
 
-    test("(update) should produce state.end when time is greater than or equal to (startTime + delay)", () => {
-        for (let delay = 0; delay < 10; delay++) {
-            for (let time = 0; time < 10; time++) {
-                const startTime = state.startTime + time + delay
-                expect(Delay.update(Delay.delay(delay), state, startTime)[1]).toEqual(state.end)
-            }
-        }
-    })
+    const delay = Delay.delay(100)
+    for (let i = state.startTime + 1; i < state.startTime + delay.duration; i++) {
+        const delayed = Delay.update(delay, state, i)[1]
 
-    test("(update) update should produce values between start and end when time is between (startTime) and (startTime + delay)", () => {
-        // we can't test for the absolute values since the easing function's implementation should remain flexible.
+        t.assert(delayed.pressure > state.start.pressure)
+        t.assert(delayed.pressure < state.end.pressure)
 
-        const delay = Delay.delay(100)
-        for (let i = state.startTime + 1; i < state.startTime + delay.duration; i++) {
-            const delayed = Delay.update(delay, state, i)[1]
+        t.assert(delayed.x > state.start.x)
+        t.assert(delayed.x < state.end.x)
 
-            expect(delayed.pressure).toBeGreaterThan(state.start.pressure)
-            expect(delayed.pressure).toBeLessThan(state.end.pressure)
+        t.assert(delayed.y > state.start.y)
+        t.assert(delayed.y < state.end.y)
+    }
+})
 
-            expect(delayed.x).toBeGreaterThan(state.start.x)
-            expect(delayed.x).toBeLessThan(state.end.x)
+test("(update) should also work for floating-point time", t => {
+    const delayed = Delay.update(Delay.delay(1), state, 0.5)[1]
 
-            expect(delayed.y).toBeGreaterThan(state.start.y)
-            expect(delayed.y).toBeLessThan(state.end.y)
-        }
-    })
+    t.assert(delayed.pressure > state.start.pressure)
+    t.assert(delayed.pressure < state.end.pressure)
 
-    test("(update) should also work for floating-point time", () => {
-        const delayed = Delay.update(Delay.delay(1), state, 0.5)[1]
+    t.assert(delayed.x > state.start.x)
+    t.assert(delayed.x < state.end.x)
 
-        expect(delayed.pressure).toBeGreaterThan(state.start.pressure)
-        expect(delayed.pressure).toBeLessThan(state.end.pressure)
+    t.assert(delayed.y > state.start.y)
+    t.assert(delayed.y < state.end.y)
+})
 
-        expect(delayed.x).toBeGreaterThan(state.start.x)
-        expect(delayed.x).toBeLessThan(state.end.x)
+test("(updateWithInput) works as expected", t => {
+    // no delay
+    const input = mkInput(600, 600, 1)
+    t.deepEqual(Delay.updateWithInput(Delay.delay(0), state, 0, input)[1], input)
+    t.deepEqual(Delay.updateWithInput(Delay.delay(0), state, 1, input)[1], input)
+    t.deepEqual(Delay.updateWithInput(Delay.delay(1), state, 0, input)[1], state.start)
+    t.deepEqual(Delay.updateWithInput(Delay.delay(1), state, 1, input)[1], input)
+})
 
-        expect(delayed.y).toBeGreaterThan(state.start.y)
-        expect(delayed.y).toBeLessThan(state.end.y)
-    })
+test("(updateWithInput) should also work for floating-point time", t => {
+    const input = mkInput(600, 600, 1)
+    const delayed = Delay.updateWithInput(Delay.delay(1), state, 0.5, input)[1]
 
-    test("(updateWithInput) works as expected", () => {
-        // no delay
-        const input = mkInput(600, 600, 1)
-        expect(Delay.updateWithInput(Delay.delay(0), state, 0, input)[1]).toEqual(input)
-        expect(Delay.updateWithInput(Delay.delay(0), state, 1, input)[1]).toEqual(input)
-        expect(Delay.updateWithInput(Delay.delay(1), state, 0, input)[1]).toEqual(state.start)
-        expect(Delay.updateWithInput(Delay.delay(1), state, 1, input)[1]).toEqual(input)
-    })
+    t.assert(delayed.pressure > state.start.pressure)
+    t.assert(delayed.pressure < input.pressure)
 
-    test("(updateWithInput) should also work for floating-point time", () => {
-        const input = mkInput(600, 600, 1)
-        const delayed = Delay.updateWithInput(Delay.delay(1), state, 0.5, input)[1]
+    t.assert(delayed.x > state.start.x)
+    t.assert(delayed.x < input.x)
 
-        expect(delayed.pressure).toBeGreaterThan(state.start.pressure)
-        expect(delayed.pressure).toBeLessThan(input.pressure)
-
-        expect(delayed.x).toBeGreaterThan(state.start.x)
-        expect(delayed.x).toBeLessThan(input.x)
-
-        expect(delayed.y).toBeGreaterThan(state.start.y)
-        expect(delayed.y).toBeLessThan(input.y)
-    })
+    t.assert(delayed.y > state.start.y)
+    t.assert(delayed.y < input.y)
 })
